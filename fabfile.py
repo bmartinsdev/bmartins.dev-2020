63712fc656ea5a4666d26363c802be9f1b75b29c
Lugh <admin@lugh.app>
2020-05-01 21:41:31 +0100

    Added fab file and a few icons

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 from fabric import Connection, task
 from invoke import run as local
 import sys
+import datetime
 sys.path.append('..')
 import variables
 lugh = Connection(
@@ -20,20 +21,20 @@ def updateProduction():
     lugh.execute("cd ~/lugh-website && git reset --hard origin/Deploy && git pull")
     lugh.execute("test -f ~/lugh-website/public_html/index.html && rm -rf ~/public_html/* && mv ~/lugh-website/public_html/* ~/public_html/")
 
-def build(msg):
-    return local('ng build --prod --output-path=../deploy/public_html && cd ../deploy && git add . && git commit -m "{}" && git push origin Deploy --force'.format(msg))
+def build():
+    currentDT = datetime.datetime.now()
+    return local('ng build --prod --output-path=../deploy/public_html && cd ../deploy && git add . && git commit -m "{}" && git push origin Deploy --force'.format(currentDT.strftime("Deployed at %Y-%m-%d %H:%M:%S")))
 
 @task 
 def setup(context):
     local("cd .. && mkdir deploy && cd deploy && git clone --single-branch --branch Deploy git@gitlab.com:brodriguespt/lugh-website.git .")
 
 @task
-def deploy(context, msg):
+def deploy(context, msg=""):
     if msg:
         commit(msg)
     else:
         print("No commit message")
-        return
-    changes = build(msg)
-    if not changes.stdout.seek('tree clean'):
-        updateProduction()
+    print("Starting build...")
+    build()
+    updateProduction()
