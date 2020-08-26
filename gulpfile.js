8e5d395941a77441e7a4c6dfee51e2f13a652aa2
Bruno Rodrigues <brunorodrigues355@gmail.com>
2020-08-26 21:37:12 +0100

    build: Swapped exec for spawn for better verbose

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,43 @@
-const gulp = require('gulp');
-const exec = require('child_process').exec;
-const { connectionString, dest, orig } = require('../credentials');
+const gulp = require("gulp");
+const spawn = require("child_process").spawn;
+const { connectionString, dest, orig } = require("../credentials");
 
 // Builds angular app
-function build (cb) {
-  exec('ng build --prod', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
+function build(cb) {
+  const building = spawn("ng", ["build", "--prod", "--verbose"]);
+  building.stdout.on("data", function (data) {
+    console.log(data.toString());
+  });
+
+  building.stderr.on("data", function (data) {
+    console.log(data.toString());
+  });
+
+  building.on("exit", function (code) {
     return cb();
   });
 }
 
 // Deploy angular app
-function sync (cb) {
-  exec(`rsync -avz --delete --progress -e "${connectionString}" ${orig} ${dest}`, function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
+function sync(cb) {
+  const syncing = spawn("rsync", [
+    "-avz",
+    "--delete",
+    "--progress",
+    "-e",
+    connectionString,
+    orig,
+    dest,
+  ]);
+  syncing.stdout.on("data", function (data) {
+    console.log(data.toString());
+  });
+
+  syncing.stderr.on("data", function (data) {
+    console.log(data.toString());
+  });
+
+  syncing.on("exit", function (code) {
     return cb();
   });
 }
